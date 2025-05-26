d467b92686ad31b83c1dae4fb8f6a6b9cd053d36
Andrew Clunis <andrew@orospakr.ca>
2025-05-26 00:07:45 -0400

    chore: create swiftpm package & sample app to depend on it

---
+++ ChordataExample/ChordataExampleTests/ChordataExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChordataExampleTests.swift
+//  ChordataExampleTests
+//
+//  Created by Andrew Clunis on 2025-05-26.
+//
+
+import Testing
+@testable import ChordataExample
+
+struct ChordataExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
